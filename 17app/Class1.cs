3e0a3d39f2d381e01ddc3f1f1ad60ee0b62450c3
spuderkon <spudermanigo@gmail.com>
2022-03-14 14:45:39 +0500

    created app

---
+++ 17app/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace _17app
+{
+    [TestFixture]
+    public class Class1
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestCase]
+        public void mainTitle()
+        {
+            driver.Url = "https://www.spacex.com/";
+            Assert.AreEqual("SpaceX",driver.Title);
+        }
+        [TearDown]
+        public void testEnd()
+        {
+            driver.Quit();
+        }
+    }
+}
